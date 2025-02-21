d35d46139a48eed49226556c6acb5e5f11969777
HidemaruOwO <owner@v-sli.me>
2025-02-21 19:03:40 +0900

    ✨ add redirects refer old pages (astro.config.mjs)

--- astro.config.mjs
+++ astro.config.mjs
@@ -20,5 +20,36 @@ export default defineConfig({
 	},
 
 	integrations: [icon(), sitemap(), minifyHtmlSwc()],
-});
 
+	redirects: {
+		"/blog/how-to-build-minecraft-server-with-docker": {
+			status: 302,
+			destination: "https://hide0.net/blog/docker-minecraft",
+		},
+		"/blog/vim-plugin-manager": {
+			status: 302,
+			destination: "https://hide0.net/blog/jetpack-plugin-manager",
+		},
+		"/blog/starrail-how-to-use-controller-with-steam": {
+			status: 302,
+			destination:
+				"https://hide0.net/blog/starrail-starrail-controller-with-steam",
+		},
+		"/blog/go-pointer": {
+			status: 302,
+			destination: "https://hide0.net/blog/go-pointer",
+		},
+		"/blog/github-ssh-key": {
+			status: 302,
+			destination: "https://hide0.net/blog/github-sshkey",
+		},
+		"/blog/mac-run-genshin-native": {
+			status: 302,
+			destination: "https://hide0.net/blog/m1-mac-genshin",
+		},
+		"/blog/linux-crlf-to-lf": {
+			status: 302,
+			destination: "https://hide0.net/blog/lf-crlf-conversion",
+		},
+	},
+});
